ba7e290e18f4a2a06f3add7316db4af4850e4ab2
student3 <student3@jh-mba158-29.d214.org>
2016-10-18 09:49:41 -0500

    Initial Commit

---
+++ JHHSAN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JHHSAN
+//
+//  Created by student3 on 10/18/16.
+//  Copyright © 2016 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
