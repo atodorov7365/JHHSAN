e1078d25068af33b95fc45344c425e21b741932c
student3 <student3@jh-mba128-01.d214.org>
2016-11-07 09:57:59 -0600

    bell schedule

--- JHHSAN/SafariViewController.swift
+++ JHHSAN/SafariViewController.swift
@@ -12,7 +12,10 @@ import SafariServices
 
 class SafariViewController: UIViewController, SFSafariViewControllerDelegate {
     var urlString = "http://jhhs.d214.org/co-curricular/activities/"
-    
+    var urlStringInfiniteCampus = "https://ic.d214.org/campus/portal/township_214.jsp"
+    var urlStringDistrictEmail = "https://gapps.d214.org/"
+    var urlStringMoodle = "http://moodle2.d214.org/"
+    var appStringSchoology = "\\:schoology"
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,7 +24,27 @@ class SafariViewController: UIViewController, SFSafariViewControllerDelegate {
     @IBAction func heresyWebSite(_ sender: UIButton) {
         let url = NSURL(string: urlString)!
         UIApplication.shared.openURL(url as URL)
+        //hersey activites
+        //  (//:) = app
+    }
+    @IBAction func infiniteCampus(_ sender: UIButton) {
+        let url = NSURL(string: urlStringInfiniteCampus)!
+        UIApplication.shared.openURL(url as URL)
+        //website
+    }
+    @IBAction func email(_ sender: UIButton) {
+        let url = NSURL(string: urlStringDistrictEmail)!
+        UIApplication.shared.openURL(url as URL)
     }
     
+    @IBAction func moodle(_ sender: UIButton) {
+        let url = NSURL(string: urlStringMoodle)!
+        UIApplication.shared.openURL(url as URL)
+    }
+    @IBAction func schoology(_ sender: UIButton) {
+        let url = NSURL(string: appStringSchoology)!
+        UIApplication.shared.openURL(url as URL)
+
+    }
     
 }
