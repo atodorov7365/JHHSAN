4ed955558c6a815d29f85f1ab1171960cb99e0c2
Crista M <Crista@Cristas-MacBook-Pro.local>
2017-04-23 19:11:25 -0500

    changes to assignment class

--- JHHSAN/AssignmentClass.swift
+++ JHHSAN/AssignmentClass.swift
@@ -1,23 +1,17 @@
 //
 //  AssignmentClass.swift
-//  JHHSAN
-//
-//  Created by Purely Coding on 1/30/17.
-//  Copyright © 2017 John Hersey High School. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class Assignment
-{
+class Assignment {
     var name : String
     var className: String
     var dateAssigned: String
     
     
-    init(name a : String, className c : String, date d : String)
-    {
+    init(name a : String, className c : String, date d : String) {
         name = a
         className = c
         dateAssigned = d
